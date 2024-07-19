493fbd813931be1248cd5e9310c8dd2856252e40
Nicholas Vanhaute <nvanhaute@wisc.edu>
2024-07-19 13:59:39 -0500

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Nicholas Vanhaute on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
